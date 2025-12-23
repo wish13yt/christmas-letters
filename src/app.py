e95bcfdb93042cd0c65397df8abb6fa7a3c07a89
Wish <wishyt@proton.me>
2025-12-23 08:44:29 -0600

    jolly

---
+++ src/app.py
@@ -0,0 +1,61 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+import discord
+from discord import app_commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()
+        print("Slash commands synced")
+
+client = MyClient(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong! Ho ho ho! Merry Christmas! 🎄🎄🎄')
+
+@client.tree.command()
+async def info(interaction: discord.Interaction):
+    """Information about licenses used by this bot and the bot itself."""
+    await interaction.response.send_message("Jolly Christmas Bot\n Jolly Christmas Bot (JCB) is open-source under the Unlicense on GitHub as wish13yt/christmas-letters. Fonts used are licensed under the SIL Open Font License, and were retreived from Google Fonts.\n The bot's profile picture is the Christmas Tree emoji, from the Noto Emoji Set that uses the Open Font License.\n You can read the OFL here: https://openfontlicense.org and the Unlicense here: https://unlicense.org/. Thank you for using JCB!")
+
+@client.tree.command()
+@app_commands.describe(
+    text='What do you want to write today?',
+    font='What font do you want? (Options: Rubik Gemstones or Open Sans) Default: Open Sans'
+)
+async def letter(interaction: discord.Interaction, text:str, font:str):
+    """Write a beautiful letter! (Note: Gets sent to whole chat)"""
+    peakfonts = ["Rubik Gemstones", "Open Sans"]
+    try:
+        img = Image.open("letter.png")
+        draw = ImageDraw.Draw(img)
+        if font == "Rubik Gemstones":
+            thefont = "Rubik_Gemstones/RubikGemstones-Regular.ttf"
+        if font == "Open Sans" or font not in peakfonts:
+            thefont = "Open_Sans/static/OpenSans-Regular.ttf"
+        font = ImageFont.truetype(thefont, 32)
+        draw.text((100, 100), text, (255,255,255), font=font)
+        img.save('generated/letter.png')
+        await interaction.response.send_message(file=discord.File("generated/letter.png"))
+    except:
+        await interaction.response.send_message("There was a very jolly issue!! Ho ho ho!")
+
+
+client.run('MTQ1MzAyMjk1NzgyMjU0NjAyMA.GbWD4r.vOvlmQxOST7Y4HOvMyKbfElnsQxDpTRFzE5urk')
